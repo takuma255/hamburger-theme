f04fb647661aba245e37595c30fbcbcbdd25b757
takuma <takuma.1268@gmail.com>
2021-10-04 22:41:22 +0900

    js-menubtn

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,6 @@
 /* サイドバーメニュー　jQuery */
 $(function(){
-  $('.js-menu').on('click',function(){
-    $('body').addClass('is-open')
-  });
-  $('.js-close').on('click',function(){
-    $('body').removeClass('is-open')
+  $('.js-menubtn').on('click',function(){
+    $('.js-sidebar').toggleClass('is-open')
   });
 }); 
