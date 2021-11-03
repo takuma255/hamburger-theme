6b503dfbb50ad998711ff191c1f01c218717f326
takuma <takuma.1268@gmail.com>
2021-11-03 15:47:49 +0900

    revert 再保存

---
+++ search.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HAMBURGER SHOP</title>
+  <meta name="description" content="沖縄の海を見ながら食べるハンバーガー。こだわりのバンズと野菜、肉本来の旨味を追求した究極のパティ。「おいしい」を「おいしい」で挟む幸せ。">
+  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=Roboto:wght@700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" href="/style.css">
+</head>
+<body class="archive-search l-body">
+  <header class="l-header p-header">
+    <button class="c-headline js-menubtn">Menu</button>
+    <div class="c-wrapper">
+      <h1 class="c-headline"><a class="c-link" href="#">Hamburger</a></h1>
+      <form class="p-search" action="#" method="POST">
+        <input class="c-searchbox--icon" type="search" name="search" placeholder="">
+        <input class="c-btn--search" type="submit" name="submit" value="検索">
+      </form><!-- /p-search -->
+    </div>
+  </header>
+  <main class="l-main p-main">
+    <div class="p-hero--archive">
+      <h1 class="p-hero__headline">Search:</h1>
+      <p class="p-hero__lead">チーズバーガー</p>
+    </div><!-- /p-hero -->
+    <article class="p-main__body--archive">
+      <h2 class="p-main__body__headline">小見出しが入ります</h2>
+      <p class="p-main__body__description">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+      <ul class="p-archive">
+        <li class="p-archive__list">
+          <figure class="l-card p-card">
+            <img src="/images/card.jpg" alt="チーズバーガー" class="l-card__img c-img">
+            <div class="l-card__body p-card__body">
+              <h3 class="p-card__body__headline">チーズバーガー</h3>
+              <h4 class="p-card__body__subheadline">小見出しが入ります</h4>
+              <figcaption class="p-card__body__description">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</figcaption>
+            </div><!-- /l-card__body p-card__body -->
+            <div class="l-card__btnarea p-card__btnarea">
+              <button class="c-btn--detail">詳しく見る</button>
+            </div><!-- /l-card__detail p-card__detail -->
+          </figure><!-- /l-card p-card -->
+        </li><!-- /p-archive__list -->
+        <li class="p-archive__list">
+          <figure class="l-card p-card">
+            <img src="/images/card.jpg" alt="ダブルチーズバーガー" class="l-card__img c-img">
+            <div class="l-card__body p-card__body">
+              <h3 class="p-card__body__headline">ダブルチーズバーガー</h3>
+              <h4 class="p-card__body__subheadline">小見出しが入ります</h4>
+              <figcaption class="p-card__body__description">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</figcaption>
+            </div><!-- /l-card__body p-card__body -->
+            <div class="l-card__btnarea p-card__btnarea">
+              <button class="c-btn--detail">詳しく見る</button>
+            </div><!-- /l-card__detail p-card__detail -->
+          </figure><!-- /l-card p-card -->
+        </li><!-- /p-archive__list -->
+        <li class="p-archive__list">
+          <figure class="l-card p-card">
+            <img src="/images/card.jpg" alt="スペシャルチーズバーガー" class="l-card__img c-img">
+            <div class="l-card__body p-card__body">
+              <h3 class="p-card__body__headline">スペシャルチーズバーガー</h3>
+              <h4 class="p-card__body__subheadline">小見出しが入ります</h4>
+              <figcaption class="p-card__body__description">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</figcaption>
+            </div><!-- /l-card__body p-card__body -->
+            <div class="l-card__btnarea p-card__btnarea">
+              <button class="c-btn--detail">詳しく見る</button>
+            </div><!-- /l-card__detail p-card__detail -->
+          </figure><!-- /l-card p-card -->
+        </li><!-- /p-archive__list -->
+        <li class="p-archive__list">
+          <figure class="l-card p-card">
+            <img src="/images/card.jpg" alt="チーズバーガー" class="l-card__img c-img">
+            <div class="l-card__body p-card__body">
+              <h3 class="p-card__body__headline">チーズバーガー</h3>
+              <h4 class="p-card__body__subheadline">小見出しが入ります</h4>
+              <figcaption class="p-card__body__description">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</figcaption>
+            </div><!-- /l-card__body p-card__body -->
+            <div class="l-card__btnarea p-card__btnarea">
+              <button class="c-btn--detail">詳しく見る</button>
+            </div><!-- /l-card__detail p-card__detail -->
+          </figure><!-- /l-card p-card -->
+        </li><!-- /p-archive__list -->
+        <li class="p-archive__list">
+          <figure class="l-card p-card">
+            <img src="/images/card.jpg" alt="チーズバーガー" class="l-card__img c-img">
+            <div class="l-card__body p-card__body">
+              <h3 class="p-card__body__headline">チーズバーガー</h3>
+              <h4 class="p-card__body__subheadline">小見出しが入ります</h4>
+              <figcaption class="p-card__body__description">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</figcaption>
+            </div><!-- /l-card__body p-card__body -->
+            <div class="l-card__btnarea p-card__btnarea">
+              <button class="c-btn--detail">詳しく見る</button>
+            </div><!-- /l-card__detail p-card__detail -->
+          </figure><!-- /l-card p-card -->
+        </li><!-- /p-archive__list -->
+      </ul><!-- /p-archive -->
+      <ul class="c-pagination">
+        <li class="c-pagination__current">page 1/10</li>
+        <li class="c-pagination__mark--prev"><a href="#" class="c-link">&laquo;</a></li>
+        <li class="c-pagination__number"><a href="#" class="c-link">1</a></li>
+        <li class="c-pagination__number"><a href="#" class="c-link">2</a></li>
+        <li class="c-pagination__number"><a href="#" class="c-link">3</a></li>
+        <li class="c-pagination__number"><a href="#" class="c-link">4</a></li>
+        <li class="c-pagination__number"><a href="#" class="c-link">5</a></li>
+        <li class="c-pagination__number"><a href="#" class="c-link">6</a></li>
+        <li class="c-pagination__number"><a href="#" class="c-link">7</a></li>
+        <li class="c-pagination__number"><a href="#" class="c-link">8</a></li>
+        <li class="c-pagination__number"><a href="#" class="c-link">9</a></li>
+        <li class="c-pagination__mark--next"><a href="#" class="c-link">&raquo;</a></li>
+      </ul><!-- /c-pagination -->
+    </article><!-- /p-main__body--archive-search -->
+  </main>
+  <div class="c-overlay js-sidebar"></div>
+  <aside class="l-sidebar p-sidebar js-sidebar">
+    <button class="c-close js-menubtn"></button>
+    <h2 class="c-headline">Menu</h2>
+    <nav class="p-sidebar__nav">
+      <ul class="p-gmenu">
+        <li class="p-gmenu__list">
+          <a class="c-link" href="#">ハンバーガー</a>
+          <ul class="p-lmenu">
+            <li class="p-lmenu__list"><a class="c-link" href="#">ハンバーガー</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">チーズバーガー</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">テリヤキバーガー</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">アボカドバーガー</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">フィッシュバーガー</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">ベーコンバーガー</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">チキンバーガー</a></li>
+          </ul>
+        </li><!-- /p-gmenu__list hamburger -->
+        <li class="p-gmenu__list">
+          <a class="c-link" href="#">サイド</a>
+          <ul class="p-lmenu">
+            <li class="p-lmenu__list"><a class="c-link" href="#">ポテト</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">サラダ</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">ナゲット</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">コーン</a></li>
+          </ul>
+        </li><!-- /p-gmenu__list side menu -->
+        <li class="p-gmenu__list">
+          <a class="c-link" href="#">ドリンク</a>
+          <ul class="p-lmenu">
+            <li class="p-lmenu__list"><a class="c-link" href="#">コーラ</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">ファンタ</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">オレンジ</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">アップル</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">紅茶（Ice/Hot）</a></li>
+            <li class="p-lmenu__list"><a class="c-link" href="#">コーヒー（Ice/Hot）</a></li>
+          </ul>
+        </li><!-- /p-gmenu__list drink -->
+      </ul>
+    </nav>
+  </aside><!-- /l-sidebar p-sidebar -->
+  <footer class="l-footer p-footer">
+    <ul class="p-footer__menu">
+      <li class="p-footer__menu__list"><a class="c-link" href="#">ショップ情報</a></li>
+      <span class="c-line--vertical"></span>
+      <li class="p-footer__menu__list"><a class="c-link" href="#">ヒストリー</a></li>
+    </ul>
+    <p class="c-copyright">Copyright:RaiseTech</p>
+  </footer>
+  <script src="/js/jquery.min.js"></script>
+  <script src="/js/sidebar.js"></script>
+</body>
+</html>
