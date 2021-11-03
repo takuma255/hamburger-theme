684df8e844d0051b37c0efe4f7b880764b5974ce
takuma <takuma.1268@gmail.com>
2021-11-03 19:19:09 +0900

    update header.phpの編集
     header.phpの編集、検索フォームのテンプレート化

--- header.php
+++ header.php
@@ -4,8 +4,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>HAMBURGER SHOP</title>
-  <meta name="description" content="沖縄の海を見ながら食べるハンバーガー。こだわりのバンズと野菜、肉本来の旨味を追求した究極のパティ。「おいしい」を「おいしい」で挟む幸せ。">
+  <title>
+    <?php bloginfo('name'); ?>
+  </title>
+  <meta name="description" content="<?php bloginfo( 'description' ); ?>">
   <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=Roboto:wght@700&display=swap"
     rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet">
@@ -16,10 +18,11 @@
   <header class="l-header p-header">
     <button class="c-headline js-menubtn">Menu</button>
     <div class="c-wrapper">
-      <h1 class="c-headline"><a class="c-link" href="#">Hamburger</a></h1>
-      <form class="p-search" action="#" method="POST">
-        <input class="c-searchbox--icon" type="search" name="search" placeholder="">
-        <input class="c-btn--search" type="submit" name="submit" value="検索">
-      </form><!-- /p-search -->
+      <h1 class="c-headline">
+        <a class="c-link" href="<?php echo esc_url(home_url('/')); ?>">
+          <?php bloginfo('name'); ?>
+        </a>
+      </h1>
+      <?php get_search_form(); ?>
     </div>
   </header>
---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form class=" p-search" action="<?php echo home_url('/'); ?>" method="get">
+  <input class="c-searchbox--icon" type="search" name="s" id="s" placeholder="">
+  <input class="c-btn--search" type="submit" name="submit" value="検索">
+</form><!-- /p-search -->
