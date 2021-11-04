f3f882c24001638c2a1b957ffcb2e10a5d9f9e2d
takuma <takuma.1268@gmail.com>
2021-11-04 20:13:16 +0900

    update titleの自動出力
    title-tagの自動出力機能を追加

--- functions.php
+++ functions.php
@@ -1,10 +1,21 @@
 <?php
-add_theme_support( 'menus');
+  add_theme_support( 'menus' );
+  add_theme_support( 'title-tag' );
+  
+  function hamburgershop_title( $title ) {
+    if( is_front_page() && is_home() ) {
+      $title = get_bloginfo( 'name', 'display' );
+    } elseif ( is_singular() ) {
+      $title = single_post_title('',false);
+    }
+      return $title;
+    }
+  add_filter( 'pre_get_document_title', 'hamburgershop_title' );
 
-function hamburgershop_script() {
-  wp_enqueue_style( 'M+PLUS+1p Roboto', '//fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=Roboto:wght@700&display=swap',array());
-  wp_enqueue_style( 'font-awesome', '//use.fontawesome.com/releases/v5.15.4/css/all.css', array(), '5.15.4');
-  wp_enqueue_style( 'hamburgershop', get_template_directory_uri(). '/css/hamburgershop.css', array(), '1.0.0');
-  wp_enqueue_style( 'style', get_template_directory_uri(). '/style.css', array(), '1.0.0');
-}
-add_action( 'wp_enqueue_scripts', 'hamburgershop_script');
+  function hamburgershop_script() {
+    wp_enqueue_style( 'M+PLUS+1p Roboto', '//fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=Roboto:wght@700&display=swap', array() );
+    wp_enqueue_style( 'font-awesome', '//use.fontawesome.com/releases/v5.15.4/css/all.css', array(), '5.15.4' );
+    wp_enqueue_style( 'hamburgershop', get_template_directory_uri(). '/css/hamburgershop.css', array(), '1.0.0' );
+    wp_enqueue_style( 'style', get_template_directory_uri(). '/style.css', array(), '1.0.0' );
+  }
+  add_action( 'wp_enqueue_scripts', 'hamburgershop_script' );
