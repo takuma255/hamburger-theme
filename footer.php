c7bc3559c583651e7b1fae6a7b13193c04b46e1d
takuma <takuma.1268@gmail.com>
2021-11-08 01:19:15 +0900

    update 固定ページnavの自動出力
    固定ページnavの自動出力

--- footer.php
+++ footer.php
@@ -1,9 +1,7 @@
 <footer class="l-footer p-footer">
   <ul class="p-footer__menu">
     <?php wp_nav_menu(); ?>
-    <li class="p-footer__menu__list"><a class="c-link" href="#">ショップ情報</a></li>
     <span class="c-line--vertical"></span>
-    <li class="p-footer__menu__list"><a class="c-link" href="#">ヒストリー</a></li>
   </ul>
   <p class="c-copyright">Copyright:RaiseTech</p>
 </footer>
