ec2942d35e16db5b0e4adf72e2977ec932b965aa
takuma <takuma.1268@gmail.com>
2021-11-09 19:18:08 +0900

    clean コードの整形
    c-line に関する記述の削除

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 <footer class="l-footer p-footer">
   <ul class="p-footer__menu">
     <?php wp_nav_menu(); ?>
-    <span class="c-line--vertical"></span>
   </ul>
   <p class="c-copyright">Copyright:RaiseTech</p>
 </footer>
