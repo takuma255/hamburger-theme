884eaa25bcf64f831afe09c989e0c4743de4e7be
takuma <takuma.1268@gmail.com>
2021-11-09 21:53:25 +0900

    update ループ処理
    ループ処理済み

--- single.php
+++ single.php
@@ -8,131 +8,15 @@
     </h1>
   </div><!-- /p-hero -->
   <article class="p-main__body--single">
-    <h2 class="p-main__body__headline">見出しh2</h2>
-    <p class="p-main__body__description">
-      Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。
-    </p>
-    <section class="p-product">
-      <h3 class="p-product__headline">見出しh3</h3>
-      <h4 class="p-product__headline--secondary">見出しh4</h4>
-      <h5 class="p-product__headline--tertiary">見出しh5</h5>
-      <h6 class="p-product__headline--quaternary">見出しh6</h6>
-      <blockquote class="c-blockquote">
-        <p class="c-blockquote__description">Blockquote
-          引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-        </p>
-        <p class="c-blockquote__source">出典元： <a class="c-link" href="#">○○○○○○○○○○○○</a></p>
-      </blockquote>
-      <div class="p-detail">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/detail--top.jpg" alt="画像の説明"
-          class="p-detail__img--top">
-        <figure class="p-detail__card">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/detail.jpg" alt="画像の説明"
-            class="p-detail__card__img">
-          <figcaption class="p-detail__card__description"> テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります
-            テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります
-            テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります
-            テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります
-            テキストが入ります テキストが入ります</figcaption>
-        </figure>
-        <figure class="p-detail__card">
-          <figcaption class="p-detail__card__description"> テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります
-            テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります
-            テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります
-            テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります
-            テキストが入ります テキストが入ります</figcaption>
-          <img src="<?php echo get_template_directory_uri(); ?>/images/detail.jpg" alt="画像の説明"
-            class="p-detail__card__img">
-        </figure>
-        <img src="<?php echo get_template_directory_uri(); ?>/images/detail.jpg" alt="画像の説明"
-          class="p-detail__img--bottom">
-      </div><!-- /l-detail p-detail -->
-    </section><!-- /p-product -->
-    <ul class="p-gallery c-layout--tile">
-      <li class="p-gallery__list">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery.jpg" alt="画像の説明" class="c-img">
-      </li>
-      <li class="p-gallery__list">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery.jpg" alt="画像の説明" class="c-img">
-      </li>
-      <li class="p-gallery__list">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery.jpg" alt="画像の説明" class="c-img">
-      </li>
-      <li class="p-gallery__list">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery.jpg" alt="画像の説明" class="c-img">
-      </li>
-      <li class="p-gallery__list">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery.jpg" alt="画像の説明" class="c-img">
-      </li>
-      <li class="p-gallery__list">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery.jpg" alt="画像の説明" class="c-img">
-      </li>
-      <li class="p-gallery__list">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery.jpg" alt="画像の説明" class="c-img">
-      </li>
-      <li class="p-gallery__list">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery.jpg" alt="画像の説明" class="c-img">
-      </li>
-      <li class="p-gallery__list">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/gallery.jpg" alt="画像の説明" class="c-img">
-      </li>
-    </ul><!-- /p-gallery -->
-    <div class="p-bullets">
-      <ol class="p-bullets__unit--ol">
-        <li class="p-bullets__unit__list">リストリストリスト</li>
-        <li class="p-bullets__unit__list">リストリストリスト</li>
-      </ol>
-      <ol class="p-bullets__unit--ol u-ml--bullets">
-        <li class="p-bullets__unit__list">リスト2リスト2リスト2</li>
-        <li class="p-bullets__unit__list">リスト2リスト2リスト2</li>
-      </ol>
-      <ol class="p-bullets__unit--ol">
-        <li class="p-bullets__unit__list">リストリストリスト</li>
-        <li class="p-bullets__unit__list">リストリストリスト</li>
-      </ol>
-      <ul class="p-bullets__unit">
-        <li class="p-bullets__unit__list">リストリストリスト</li>
-        <li class="p-bullets__unit__list">リストリストリスト</li>
-      </ul>
-      <ul class="p-bullets__unit u-ml--bullets">
-        <li class="p-bullets__unit__list">リスト2リスト2リスト2</li>
-        <li class="p-bullets__unit__list">リスト2リスト2リスト2</li>
-      </ul>
-      <ul class="p-bullets__unit">
-        <li class="p-bullets__unit__list">リストリストリスト</li>
-        <li class="p-bullets__unit__list">リストリストリスト</li>
-      </ul>
-    </div><!-- /p-bullets -->
-    <pre class="c-code">
-        <code class="c-code__txt">
-&lt;html&gt;
-        &lt;head&gt;
-        &lt;/head&gt;
-        &lt;body&gt;
-        &lt;/body&gt;
-&lt;/html&gt;
-        </code>
-      </pre>
-    <table class="c-table">
-      <tr class="c-table__row">
-        <td class="c-table__row__data--left">テーブル</td>
-        <td class="c-table__row__data--right">テーブル</td>
-      </tr>
-      <tr class="c-table__row">
-        <td class="c-table__row__data--left">テーブル</td>
-        <td class="c-table__row__data--right">テーブル</td>
-      </tr>
-      <tr class="c-table__row">
-        <td class="c-table__row__data--left">テーブル</td>
-        <td class="c-table__row__data--right">テーブル</td>
-      </tr>
-      <tr class="c-table__row">
-        <td class="c-table__row__data--left">テーブル</td>
-        <td class="c-table__row__data--right">テーブル</td>
-      </tr>
-    </table>
-    <button class="c-btn">ボタン</button>
-    <p class="c-tag">boldboldboldboldboldboldbold</p>
+    <?php
+      if( have_posts()): ?>
+    <?php
+        while( have_posts()): the_post(); ?>
+    <?php the_content(); ?>
+    <?php endwhile;
+      else :
+      ?><p class="p-single__error">該当商品が見つかりません</p>
+    <?php endif; ?>
   </article><!-- /p-main__body--single -->
 </main>
 <div class="c-overlay js-sidebar"></div>
