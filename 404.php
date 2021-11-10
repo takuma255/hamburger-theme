9336f96196aaf1768ba2ceab2d80c964a556b658
takuma <takuma.1268@gmail.com>
2021-11-11 01:12:01 +0900

    update 404,faviconの追加
    404,faviconの追加

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+  get_header(); ?>
+
+<main class="l-main p-main">
+  <article class="p-main__body--archive">
+    <h2 class="p-main__body__headline">
+      404error : お探しのページが見つかりません
+    </h2>
+  </article><!-- /p-main__body--archive -->
+</main>
+<div class="c-overlay js-sidebar"></div>
+
+<?php
+  get_sidebar(); ?>
+
+<?php
+  get_footer(); ?>
--- header.php
+++ header.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri().'/favicon.png'; ?>">
   <?php wp_head(); ?>
 </head>
 
